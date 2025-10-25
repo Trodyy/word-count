62baf4487eeb8d2dfa604972ea1ea7a0bd0a16f4
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-10-25 06:42:26 +0330

    FIX : Read file in batch mode.

--- python/main.py
+++ python/main.py
@@ -1,3 +1,12 @@
+import threading
+
 with open("/home/trody/code/projects/word-count/test.txt", "r") as file:
     text = file.read()
-    print(text)
+
+def reading_file(file_path , batch_size = 1024*1024) : 
+    with open(file_path , 'r' , encoding='utf-8') as f :
+        while True :
+            data = f.read(batch_size)
+            if not data :
+                break
+            yield data
