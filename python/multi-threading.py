f30069aae0d19dc88d10e0603b3a24fe67f3025a
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-11-13 10:47:15 +0330

    FEAT : Initilize multi-threading.py file.

---
+++ python/multi-threading.py
@@ -0,0 +1,25 @@
+import threading
+from collections import Counter
+
+def count_words(text_part, result, index):
+    words = text_part.split()
+    result[index] = Counter(words)
+
+text = open("Example.txt").read()
+parts = text.split("\n\n")
+result = [None] * len(parts)
+threads = []
+
+for i, part in enumerate(parts):
+    t = threading.Thread(target=count_words, args=(part, result, i))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+final_count = Counter()
+for r in result:
+    final_count.update(r)
+
+print(final_count)
